900a5cd349d06c0c8499fe9ec60edadf456a0e81
Deilicis <118275222+Deilicis@users.noreply.github.com>
2024-05-27 21:28:04 +0300

    Realizēta galvenais UI logs un uzsākts info logs

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,43 +1,106 @@
 package main;
 
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.WindowConstants;
 
 public class UI extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel mainPanel = new JPanel();
+    private JPanel infoPanel = new JPanel();
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+    Image bckImg = new ImageIcon(this.getClass().getResource("/background.jpg")).getImage();
 
-	public UI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1275, 642);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public UI() {
+        initialize();
+        setupInfoPanel();
+        setContentPane(cardPanel);
+    }
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel galvenaisTitulsLbl = new JLabel("Sazarojuma konstrukcijas programmēšanas valodā Java");
-		galvenaisTitulsLbl.setFont(new Font("Sitka Text", Font.PLAIN, 30));
-		galvenaisTitulsLbl.setBounds(254, 66, 797, 87);
-		contentPane.add(galvenaisTitulsLbl);
-		
-		JLabel testsLbl = new JLabel("TESTS");
-		testsLbl.setFont(new Font("Sitka Text", Font.PLAIN, 40));
-		testsLbl.setBounds(567, 10, 120, 60);
-		contentPane.add(testsLbl);
-		
-		JButton saktBtn = new JButton("Sākt");
-		saktBtn.setBounds(400, 291, 408, 73);
-		contentPane.add(saktBtn);
-		
-		JButton izietBtn = new JButton("Iziet");
-		izietBtn.setBounds(400, 403, 408, 73);
-		contentPane.add(izietBtn);
-	}
+    private void initialize() {
+        setTitle("Picērija");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setBounds(100, 100, 1275, 642);
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel();
+        cardPanel.setLayout(cardLayout);
+
+        mainPanel.setLayout(null);
+
+        JLabel galvenaisTitulsLbl = new JLabel("Sazarojuma konstrukcijas programmēšanas valodā Java");
+        galvenaisTitulsLbl.setFont(new Font("Sitka Text", Font.PLAIN, 30));
+        galvenaisTitulsLbl.setBounds(254, 66, 797, 87);
+        mainPanel.add(galvenaisTitulsLbl);
+
+        JLabel testsLbl = new JLabel("TESTS");
+        testsLbl.setFont(new Font("Sitka Text", Font.PLAIN, 40));
+        testsLbl.setBounds(567, 10, 120, 60);
+        mainPanel.add(testsLbl);
+
+        JButton saktBtn = new JButton("Sākt");
+        saktBtn.setFont(new Font("Sitka Text", Font.PLAIN, 30));
+        saktBtn.setBounds(484, 305, 241, 60);
+        mainPanel.add(saktBtn);
+
+        JButton izietBtn = new JButton("Iziet");
+        izietBtn.setFont(new Font("Sitka Text", Font.PLAIN, 30));
+        izietBtn.setBounds(484, 404, 241, 73);
+        mainPanel.add(izietBtn);
+
+        JLabel autorLbl = new JLabel("Autors: Dāvis Zigners");
+        autorLbl.setFont(new Font("Sitka Text", Font.PLAIN, 15));
+        autorLbl.setBounds(1099, 576, 162, 29);
+        mainPanel.add(autorLbl);
+
+        JLabel imgLbl = new JLabel();
+        imgLbl.setIcon(new ImageIcon(bckImg));
+        imgLbl.setBounds(-138, -312, 1434, 1125);
+        mainPanel.add(imgLbl);
+
+        cardPanel.add(mainPanel, "mainPanel");
+
+        saktBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "infoPanel");
+            }
+        });
+
+        izietBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    private void setupInfoPanel() {
+        infoPanel.setLayout(null);
+        infoPanel.setBackground(Color.WHITE);
+
+        JLabel infoLabel = new JLabel("Informācija par šo testu");
+        infoLabel.setFont(new Font("Sitka Text", Font.PLAIN, 30));
+        infoLabel.setBounds(100, 100, 500, 50);
+        infoPanel.add(infoLabel);
+        
+        JLabel imgLbl = new JLabel();
+        imgLbl.setIcon(new ImageIcon(bckImg));
+        imgLbl.setBounds(-138, -312, 1434, 1125);
+        infoPanel.add(imgLbl);
+
+        cardPanel.add(infoPanel, "infoPanel");
+    }
 }

ebd0e8fd1ab025a25c6e50f89b6c79933588e21e
Deilicis <118275222+Deilicis@users.noreply.github.com>
2024-05-27 22:01:42 +0300

    Šodienas pēdējais commit, ģeneŗēju idejas jautājumiem.

--- src/main/Klase.java
+++ src/main/Klase.java
@@ -1,10 +1,13 @@
 package main;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 public class Klase {
-
+	static ArrayList<Uzdevumi> uzdArray = new ArrayList<Uzdevumi>();
 	public static void main(String[] args) {
+		String[] var1 = {"6", "8", "9", "10"};
+		uzdArray.add(new Uzdevumi("Kādi būs šī koda izvaddati?", 1, null, var1));
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
--- src/main/UI.java
+++ src/main/UI.java
@@ -12,7 +12,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.WindowConstants;
 
 public class UI extends JFrame {
